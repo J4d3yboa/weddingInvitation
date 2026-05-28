18348f0820d58e7584234166f8f4e5fbe8059a4a
J4d3yboa <jadeyasemin220304@gmail.com>
2026-05-28 18:44:18 +0200

    React gone

--- src/components/WeddingInvitation.tsx
+++ src/components/WeddingInvitation.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { MapPin, Calendar, Clock, Phone, Bed, Car, Cake } from 'lucide-react';
 import  strandImage  from "../assets/Strand.png";
